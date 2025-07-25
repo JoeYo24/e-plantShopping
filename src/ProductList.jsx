7392898e7541d280d613aab594b400e49d19a48b
JoeYo24 <jjsteffen@hotmail.com>
2025-07-25 17:13:26 -0400

    did some more changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({})
+    const dispatch = useDispatch();
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -253,12 +256,12 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
 
-    const handleAddToCart = (e) => {
-        dispatchEvent(addItem(product));
+    const handleAddToCart = (plant) => {
+        dispatch(addItem(plant));
 
         setAddedToCart((prevState) => ({
             ...prevState,
-            [product.name]: true,
+            [plant.name]: true,
         }));
     };
     return (
